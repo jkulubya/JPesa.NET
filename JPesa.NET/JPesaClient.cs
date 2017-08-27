902290aa013f07624d8c55aaeab7fd76c349a2f1
James Kitamirike <jameskitamirike@gmail.com>
2017-08-27 15:50:05 +0200

    Initial

---
+++ JPesa.NET/JPesaClient.cs
@@ -0,0 +1,146 @@
+﻿using Flurl;
+using System;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading.Tasks;
+using JPesa.NET.Models;
+using Newtonsoft.Json;
+
+namespace JPesa.NET
+{
+    public class JpesaClient
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public JpesaClient()
+        {
+            
+        }
+
+        /// <summary>
+        /// Initialize a new Jpesa Client with the specified user name and
+        /// password.
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        public JpesaClient(string userName, string password)
+        {
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                throw new ArgumentNullException(userName, "The user name cannot be empty");
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentNullException(password, "The password cannot be empty");
+            }
+            Password = password;
+            UserName = userName;
+        }
+
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        protected string JpesaUri { get; set; } = "https://secure.jpesa.com/api.php";
+
+        /// <summary>
+        /// Check the status of a previous pull or push transaction with the
+        /// specified transaction id.
+        /// </summary>
+        /// <param name="transactionId"></param>
+        /// <returns></returns>
+        public async Task<Transaction> QueryTransaction(string transactionId)
+        {
+            if (string.IsNullOrEmpty(transactionId))
+            {
+                throw new ArgumentNullException(transactionId, "The transaction ID cannot be null");
+            }
+
+            var url = JpesaUri.SetQueryParams(new
+            {
+                action="info",
+                command="jpesa",
+                IS_GET = "3",
+                password=Password,
+                tid=transactionId,
+                username = UserName,
+            });
+            
+            Console.WriteLine(url.ToString());
+            string response;
+            using (var httpClient = new HttpClient())
+            {
+                response = await httpClient.GetStringAsync(url.ToString());
+            }
+            if (response.Contains("SUCCESS"))
+            {
+                var result =
+                    JsonConvert.DeserializeObject<Transaction>(
+                        response.Substring(response.IndexOf("{", StringComparison.Ordinal)));
+                return result;
+            }
+            else
+            {
+                var result = response.Substring(response.IndexOf(" ", StringComparison.Ordinal) + 1);
+                throw new Exception(result);
+            }
+        }
+
+        /// <summary>
+        /// This method informs JPesa to pull the specified amount of mobile 
+        /// money from the specified phone number into the account 
+        /// associated with this JpesaClient. Check the status of the 
+        /// transaction with the QueryTransaction method.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="phoneNumber"></param>
+        /// <returns>The id of the created trasanction</returns>
+        public async Task<string> PullMobileMoney(decimal amount, string phoneNumber)
+        {
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                throw new ArgumentNullException(phoneNumber, "The transaction ID cannot be null");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    amount.ToString(CultureInfo.InvariantCulture), "The amount must be greater than zero");
+            }
+
+            var url = JpesaUri.SetQueryParams(new
+            {
+                action = "deposit",
+                amount,
+                command = "jpesa",
+                IS_GET = "3",
+                number = phoneNumber,
+                password = Password,
+                username = UserName,
+            });
+
+            string response;
+            using (var httpClient = new HttpClient())
+            {
+                response = await httpClient.GetStringAsync(url.ToString());
+            }
+
+            if (response.Contains("[SUCCESS]"))
+            {
+                var txnId = response.Substring(response.IndexOf("|", StringComparison.Ordinal) + 1);
+                return txnId;
+            }
+            throw new Exception(response.Substring(response.IndexOf(" ", StringComparison.Ordinal) + 1));
+        }
+
+        ///// <summary>
+        ///// Attempts to send the specified amount of mobile moeny from the
+        ///// account associated with this JpesaClient to the specified phone
+        ///// number.
+        ///// </summary>
+        ///// <param name="amount"></param>
+        ///// <param name="phoneNumber"></param>
+        //public void SendMobileMoney(decimal amount, string phoneNumber)
+        //{
+        //}
+    }
+}
---
+++ JPesa.NET/Models/Transaction.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+
+namespace JPesa.NET.Models
+{
+    public class Transaction
+    {
+        /// <summary>
+        /// Internal JPesa id of transaction.
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The total amount requested in the transaction. NB: This is the 
+        /// total sum without regard to fees. To get the actual sum that
+        /// is received or deposited, make sure to subtract the fees.
+        /// </summary>
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// The fee charged by JPesa for this transaction.
+        /// </summary>
+        [JsonProperty("fee")]
+        public decimal Fee { get; set; }
+
+        /// <summary>
+        /// The three letter currency code for the amount and fees in this
+        /// transaction.
+        /// </summary>
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        /// <summary>
+        /// The status of this transaction.
+        /// </summary>
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        /// <summary>
+        /// The identifier for the mobile network from/to which the mobile
+        /// money was withdrawn/deposited.
+        /// </summary>
+        [JsonProperty("pp")]
+        public string MobileNetwork { get; set; }
+
+        /// <summary>
+        /// The phone number of the mobile wallet from/to which the mobile
+        /// money was deposited.
+        /// </summary>
+        [JsonProperty("number")]
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// The public id of the transaction. Can be used to track the status
+        /// of a transaction via Instant Payment Notification.
+        /// </summary>
+        [JsonProperty("tid")]
+        public string TransactionId { get; set; }
+
+        /// <summary>
+        /// Unix timestamp in seconds of when the transaction was initiated.
+        /// </summary>
+        [JsonProperty("timestamp")]
+        public int Timestamp { get; set; }
+    }
+}
